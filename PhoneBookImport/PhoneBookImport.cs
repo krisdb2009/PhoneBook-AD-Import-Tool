a00b35615dd3bd01a5274ceb9ebbf2464f473fa8
dylan <dylan@belowaverage.org>
2018-06-09 14:48:46 -0400

    Added tag manager

--- PhoneBookImport/PhoneBookImport.cs
+++ PhoneBookImport/PhoneBookImport.cs
@@ -13,7 +13,72 @@ namespace PhoneBookImport
     class Router
     {
         static List<List<string>> translation = new List<List<string>>();
+        static List<string> descriptionScheme = new List<string>();
+        static List<string> ldapTags = new List<string>();
+        static DirectorySearcher dSearch;
+        static string ldapNumberAttribute;
+
         static void Main(string[] args)
+        {
+            init();
+
+            SearchResultCollection results = dSearch.FindAll();
+            foreach (SearchResult result in results)
+            {
+                if (result.Properties[ldapNumberAttribute].Count > 0)
+                {
+                    Console.WriteLine("\n" + result.Path);
+                    Console.WriteLine(generatePhoneNumber(result));
+                    Console.WriteLine(generateDescription(result));
+                    foreach (string tag in generateTags(result))
+                    {
+                        Console.WriteLine(tag);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public static string generatePhoneNumber(SearchResult searchResult)
+        {
+            string number = "";
+            if(searchResult.Properties[ldapNumberAttribute].Count != 0)
+            {
+                string num = searchResult.Properties[ldapNumberAttribute][0].ToString();
+                foreach (char character in num)
+                {
+                    if (!char.IsNumber(character))
+                    {
+                        num = num.Replace(character, char.Parse(""));
+                    }
+                }
+                number = num;
+            }
+            return number;
+        }
+
+        public static string generateDescription(SearchResult searchResult)
+        {
+            int count = 0;
+            string description = "";
+            foreach(string scheme in descriptionScheme)
+            {
+                if(count++ % 2 == 1)
+                {
+                    if(searchResult.Properties[scheme].Count != 0)
+                    {
+                        description = description + searchResult.Properties[scheme][0].ToString();
+                    }
+                }
+                else
+                {
+                    description = description + scheme;
+                }
+            }
+            return description;
+        }
+
+        public static void init()
         {
             //If configuration does not exits, create one.
             if (!File.Exists("PhoneBookImport.exe.config"))
@@ -23,33 +88,32 @@ namespace PhoneBookImport
             }
 
             //Pull configuration.
-            string ldapNumberAttribute = ConfigurationManager.AppSettings["ldapNumberAttribute"];
+            ldapNumberAttribute = ConfigurationManager.AppSettings["ldapNumberAttribute"];
             string API = ConfigurationManager.AppSettings["API"];
             string descriptionString = ConfigurationManager.AppSettings["descriptionString"];
             string tagList = ConfigurationManager.AppSettings["tagList"];
             string translationList = ConfigurationManager.AppSettings["translationList"];
 
             //Setup the directory searcher using the filter specified in the config.
-            DirectorySearcher dSearch = new DirectorySearcher(ConfigurationManager.AppSettings["ldapFilter"]);
-
-            List<string> tags = new List<string>();
+            dSearch = new DirectorySearcher(ConfigurationManager.AppSettings["ldapFilter"]);
 
             foreach (string tag in tagList.Split(char.Parse(",")))
             {
                 string ldapAttr = tag.TrimStart(char.Parse(" "));
-                if(!tags.Contains(ldapAttr))
+                if (!ldapTags.Contains(ldapAttr))
                 {
                     dSearch.PropertiesToLoad.Add(ldapAttr);
-                    tags.Add(ldapAttr);
+                    ldapTags.Add(ldapAttr);
                 }
             }
             int lineCount = 0;
-            foreach (string ldapAttr in descriptionString.Split(char.Parse("%")))
+            foreach (string scheme in descriptionString.Split(char.Parse("%")))
             {
-                if (lineCount++ % 2 == 1 && !ldapAttr.Contains(" "))
+                if (lineCount++ % 2 == 1 && !scheme.Contains(" "))
                 {
-                    dSearch.PropertiesToLoad.Add(ldapAttr);
+                    dSearch.PropertiesToLoad.Add(scheme);
                 }
+                descriptionScheme.Add(scheme);
             }
             foreach (string translation in translationList.Split(char.Parse(",")))
             {
@@ -65,46 +129,42 @@ namespace PhoneBookImport
                 }
                 Router.translation.Add(list);
             }
-
             dSearch.PageSize = 10000;
             dSearch.PropertiesToLoad.Add(ldapNumberAttribute);
             dSearch.SearchRoot = new DirectoryEntry("LDAP://" + ConfigurationManager.AppSettings["ldapSearchRoot"]);
-            SearchResultCollection results = dSearch.FindAll();
-            foreach (SearchResult result in results)
-            {
-                if (result.Properties[ldapNumberAttribute].Count > 0)
-                {
-                    Console.WriteLine("\n" + result.Path);
-                    //Console.WriteLine(result.Properties[ldapNumberAttribute][0] + "\n");
+        }
 
-                    foreach (string t in tags)
+        public static List<string> generateTags(SearchResult searchResult)
+        {
+            List<string> finalTags = new List<string>();
+            foreach (string t in ldapTags)
+            {
+                if (searchResult.Properties[t].Count != 0)
+                {
+                    string tagString = searchResult.Properties[t][0].ToString();
+                    foreach (char character in tagString)
                     {
-                        if(result.Properties[t].Count != 0)
+                        if (!char.IsLetter(character))
                         {
-                            string tagString = result.Properties[t][0].ToString();
-                            foreach (char character in tagString)
+                            tagString = tagString.Replace(character, char.Parse(" "));
+                        }
+                    }
+                    foreach (string tag in tagString.Split(char.Parse(" ")))
+                    {
+                        if (tag != "")
+                        {
+                            foreach (string translatedTag in tagTranslator(tag))
                             {
-                                if (!char.IsLetter(character))
+                                if (!finalTags.Contains(translatedTag))
                                 {
-                                    tagString = tagString.Replace(character, char.Parse(" "));
-                                }
-                            }
-                            foreach(string tag in tagString.Split(char.Parse(" ")))
-                            {
-                                if(tag != "")
-                                {
-                                    foreach(string translatedTag in tagTranslator(tag))
-                                    {
-                                        Console.WriteLine(translatedTag);
-                                    }
+                                    finalTags.Add(translatedTag);
                                 }
                             }
                         }
                     }
                 }
             }
-
-            Console.ReadLine();
+            return finalTags;
         }
 
         public static List<string> tagTranslator(string tag)
@@ -113,21 +173,23 @@ namespace PhoneBookImport
             bool found = false;
             foreach(List<string> list in translation)
             {
-                if(list[0].ToLower() == tag.ToLower())
+                string lowerTag = tag.ToLower();
+                if(list[0].ToLower() == lowerTag)
                 {
                     found = true;
                     foreach(string replacementTag in list)
                     {
-                        if(replacementTag.ToLower() != tag.ToLower())
+                        string lowerReplacementTag = replacementTag.ToLower();
+                        if (lowerReplacementTag != lowerTag)
                         {
-                            translatedTags.Add(replacementTag);
+                            translatedTags.Add(lowerReplacementTag);
                         }
                     }
                 }
             }
             if(!found)
             {
-                translatedTags.Add(tag);
+                translatedTags.Add(tag.ToLower());
             }
             return translatedTags;
         }
